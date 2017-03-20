7549340a1e63b981c5fd69221a0b3d728912e64a
Oleg Goncharenko <luckydev3000@gmail.com>
2017-03-20 10:22:28 -0600

    first

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+       System.out.println(getCurrentDate("yearFirst"));
+    }
+
+    public static String getCurrentDate(String dateFormat){
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter dtf;
+
+        switch (dateFormat){
+            case "yearLast":
+                dtf = DateTimeFormatter.ofPattern("MM/dd/y");
+                break;
+            case "yearFirst":
+                dtf = DateTimeFormatter.ofPattern("yMMdd");
+                break;
+            default:
+                throw new IllegalArgumentException("dateFormat argument is illegal");
+        }
+        return dtf.format(date);
+    }
+}
